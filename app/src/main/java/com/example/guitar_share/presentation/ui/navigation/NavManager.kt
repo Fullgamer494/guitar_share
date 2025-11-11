392931afebbc4e8afe85f502b5a5fbe4ac864ff1
Fullgamer494 <fullgamer494@hotmail.com>
2025-11-11 14:28:47 -0600

    Implementar barra de navegación flotante

---
+++ app/src/main/java/com/example/guitar_share/presentation/ui/navigation/NavManager.kt
@@ -0,0 +1,35 @@
+package com.example.guitar_share.presentation.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.guitar_share.presentation.ui.screens.home.HomeScreen
+
+@Composable
+fun NavManager(){
+    val navController = rememberNavController()
+
+    NavHost(
+        navController,
+        startDestination = "home"
+    ){
+        composable("score"){
+
+        }
+
+        composable("library"){
+
+        }
+
+        composable("home"){
+            HomeScreen(navController)
+        }
+
+        composable("forum"){
+        }
+
+        composable("profile"){
+        }
+    }
+}
