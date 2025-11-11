9dbe65b918ea0090d4ea92d4ec0028942f8e5ec9
Fullgamer494 <fullgamer494@hotmail.com>
2025-11-11 14:29:32 -0600

    Añadir archivo HomeScreen.kt

--- app/src/main/java/com/example/guitar_share/MainActivity.kt
+++ app/src/main/java/com/example/guitar_share/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.guitar_share.presentation.ui.navigation.NavManager
 import com.example.guitar_share.presentation.ui.theme.Guitar_shareTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +13,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Guitar_shareTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                NavManager()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Guitar_shareTheme {
-        Greeting("Android")
     }
 }
