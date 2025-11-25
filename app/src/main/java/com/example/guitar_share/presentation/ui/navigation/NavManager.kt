62aea622b698df576f4d2215e2cb556ae8193cbb
Ilyavosky <ilyacortesruiz@gmail.com>
2025-11-24 22:37:56 -0600

    Corregir ruta de los foros

--- app/src/main/java/com/example/guitar_share/presentation/ui/navigation/NavManager.kt
+++ app/src/main/java/com/example/guitar_share/presentation/ui/navigation/NavManager.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.guitar_share.presentation.ui.screens.forum.ForumScreen
 import com.example.guitar_share.presentation.ui.screens.home.HomeScreen
 import com.example.guitar_share.presentation.ui.screens.scores_library.AddNewScore
 import com.example.guitar_share.presentation.ui.screens.scores_library.ScoreLibraryScreen
@@ -31,6 +32,7 @@ fun NavManager(){
         }
 
         composable("forum"){
+            ForumScreen(navController)
         }
 
         composable("profile"){
