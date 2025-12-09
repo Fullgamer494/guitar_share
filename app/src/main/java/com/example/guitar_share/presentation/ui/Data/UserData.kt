8b1ff16ccd7dcb020de9456297ae5aed460d2d90
Fullgamer494 <fullgamer494@gmail.com>
2025-12-09 12:14:20 -0600

    Eliminar código de interfaz de usuario antiguo (ui)

--- app/src/main/java/com/example/guitar_share/presentation/ui/Data/UserData.kt
+++
@@ -1,7 +0,0 @@
-package com.example.guitar_share.presentation.ui.Data
-
-data class User(
-    val name: String,
-    val email: String,    val memberSince: String,
-    val profileImageUrl: String
-)
